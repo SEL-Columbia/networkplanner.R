641f5d7d8aacbdefa9fa649eced9952168f929fb
prabhasp <prabhas.pokharel@gmail.com>
2014-02-21 18:06:03 -0500

    v0.0.1

---
+++ R/networkplanner.R
@@ -0,0 +1,11 @@
+
+#' Download scenario from networkplanner into the given directory
+#'
+#' @param scenario_number scenario_number on http://networkplanner.modilabs.org
+#' @param directory_name path to write the downloaded scenario into. By default,
+#'        and if directory_name==NULL, directory_name will be scenario number within
+#'        current working directory
+#' @export
+download_scenario = function(scenario_number, directory_name=NULL) {
+    stop("Not Implemneted")
+}
