19bdc18a7f1f1c4a7c184f7b11bb75086fa2997a
prabhasp <prabhas.pokharel@gmail.com>
2014-02-25 17:23:26 -0500

    beginning read_networkplan

--- R/networkplanner.R
+++ R/networkplanner.R
@@ -1,3 +1,9 @@
+require(sp)
+require(igraph)
+
+# The NetworkPlan object
+setClass("NetworkPlan", representation(nodes="SpatialPointsDataFrame",
+                                       network="igraph", existing_network="SpatialLinesDataFrame"))
 
 #' Download scenario from networkplanner into the given directory
 #'
@@ -5,7 +11,24 @@
 #' @param directory_name path to write the downloaded scenario into. By default,
 #'        and if directory_name==NULL, directory_name will be scenario number within
 #'        current working directory
+#' @param userpwd username and password, separated by : (ex. USER:PASSWORD). If NULL,
+#'        we assume that the scenario is public
+#' @param np_url URL of the network planner instance to download scenario from. By default,
+#'        it is http://networkplanner.modilabs.org
 #' @export
-download_scenario = function(scenario_number, directory_name=NULL) {
-    stop("Not Implemneted")
+download_scenario = function(scenario_number, directory_name=NULL, userpwd=NULL, 
+                             np_url='http://networkplanner.modilabs.org/') {
+    stop("Not Implemented")
+}
+
+#' Read network plan from a directory in the filesystem
+#' 
+#' @param directory_name absolute or relative path to a directory from which a NetworkPlan is loaded
+#' @return A NetworkPlan object
+#' @export
+read_networkplan = function(directory_name) {
+    base_dir = normalizePath(directory_name)
+    metrics_csv <- read.csv(file.path(base_dir, "metrics-local.csv"), skip=1)
+    proj4string <- str_split(readLines(file.path(base_dir, "metrics-local.csv"), n=1), ",")[[1]][[1]]
+    
 }
