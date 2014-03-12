de506cefab2959215bc544bb8ba37b5c9b989648
zmyao88 <zm.yao88@gmail.com>
2014-03-12 18:25:08 -0400

    fix bug in Chris' work and refactord another version

--- R/np-util.R
+++ R/np-util.R
@@ -23,13 +23,47 @@ get_adjacency_matrix <- function(line_matrix, coord_df) {
     adj_matrix[1:nrow(coord_df), 1:nrow(coord_df)] <- 0 
     # must be a better way to set the adjacency matrix incidence cells
     # but for now, use index_matrices (both from/to and to/from)
-    index_array1 <- array(c(p1_ids, p2_ids), dim=c(length(p1_ids), 2))
-    index_array2 <- array(c(p2_ids, p1_ids), dim=c(length(p1_ids), 2))
+    index_array1 <- array(as.integer(c(p1_ids, p2_ids)), dim=c(length(p1_ids), 2))
+    index_array2 <- array(as.integer(c(p2_ids, p1_ids)), dim=c(length(p1_ids), 2))
     adj_matrix[index_array1] <- 1
     adj_matrix[index_array2] <- 1
     adj_matrix
 }
- 
+
+get_adjacency_matrix <- function(network_shp){
+    line_matrix <- get_coord_matrix(network_shp)
+    p1 <- line_matrix[1:dim(line_matrix)[1],1,1:2]
+    p2 <- line_matrix[1:dim(line_matrix)[1],2,1:2]
+    
+    # Merge with node df and detecting ghost nodes & roots
+    t1 <- match(data.frame(t(p1)), data.frame(t(nodes@coords)))
+    t2 <- match(data.frame(t(p2)), data.frame(t(nodes@coords)))
+    match_result <- cbind(t1,t2)
+    
+    # merge with coordinate matrix(unique) and get the rowname/id
+    coord_df<- data.frame(t(unique(rbind(p1,p2))))
+    index_array1 <- cbind(match(data.frame(t(p1)), coord_df), 
+                     match(data.frame(t(p2)), coord_df))
+    index_array2 <- cbind(match(data.frame(t(p2)), coord_df),
+                          match(data.frame(t(p1)), coord_df))
+    
+    
+    # Creating the adj matrix
+    adj_matrix <- matrix(nrow=ncol(coord_df), ncol=ncol(coord_df), data=0)
+    adj_matrix[index_array1] <- 1
+    adj_matrix[index_array2] <- 1
+    
+    return(adj_matrix)
+}
+
+
+my_graph <- graph.adjacency(adj_matrix, mode="undirected")
+plot(my_graph)
+
+
+
+
+
 # Get the co-ordinate matrix out of a SpatialLinesDataFrame (as a three dimensional array)
 # Invariant: we should be connecting straight lines in 2D space (ie, 2nd + 3rd dims are 2)
 get_coord_matrix = function(sldf) {
@@ -41,7 +75,7 @@ get_coord_matrix = function(sldf) {
             my_coords <- segment@coords
             n <- dim(my_coords)[1]
             if(n == 2){
-                array(data=t(my_coords), dim=c(1,2,2))
+                array(data=my_coords, dim=c(1,2,2))
             }else{
                 laply(1:(n-1), function(row_num){
                     cbind(my_coords[row_num, ], my_coords[row_num+1, ])
@@ -75,9 +109,3 @@ get_coord_matrix = function(sldf) {
 
 
 # To Do: combine the following chunk with adjancey matrix function to increase speed
-# coord_matrix <- get_coord_matrix(network_shp)
-# p1 <- coord_matrix[1:dim(coord_matrix)[1],1,1:2]
-# p2 <- coord_matrix[1:dim(coord_matrix)[1],2,1:2]
-# t1 <- match(p1, nodes@coords)
-# t2 <- match(p2, nodes@coords)
-# match_result <- cbind(t1,t2)
