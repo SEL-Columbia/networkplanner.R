fdacd1ecef27071b132bd807a7b46c109f41de44
chrisnatali <chris.natali@gmail.com>
2014-03-14 19:22:49 -0400

    basic structure of create_graph and updated read_networkplan

--- R/networkplanner.R
+++ R/networkplanner.R
@@ -16,8 +16,8 @@ setClass("NetworkPlan", representation(nodes="SpatialPointsDataFrame",
 # Handle case with an existing_network as a subclass for now 
 # There's probably a better way to handle it, but without this R complains
 # about the existing_network slot being empty if there's nothing there
-setClass("NetworkPlanEx", representation(existing_network="SpatialLinesDataFrame"),
-                                        contains="NetworkPlan")
+# setClass("NetworkPlanEx", representation(existing_network="SpatialLinesDataFrame"),
+#                                        contains="NetworkPlan")
 
 
 #' Download scenario from networkplanner into the given directory
@@ -46,45 +46,32 @@ read_networkplan = function(directory_name, debug=F) {
     base_dir = normalizePath(directory_name)
     
     # read nodes and assign id
-    metrics_csv <- read.csv(file.path(base_dir, "metrics-local.csv"), skip=1)
+    metrics_df <- read.csv(file.path(base_dir, "metrics-local.csv"), skip=1)
     proj4string <- str_extract(readLines(file.path(base_dir, "metrics-local.csv"), n=1), "[+][^,]*")
-    nodes <- SpatialPointsDataFrame(coords=metrics_csv[,c("X","Y")], data=metrics_csv,
+    nodes <- SpatialPointsDataFrame(coords=metrics_df[,c("X","Y")], data=metrics_df,
                                     proj4string=CRS(proj4string))
     nodes$id <- as.numeric(row.names(nodes)) # note: this is assumed in the matching code later
+    
     # read network
     network_shp <- readOGR(dsn=base_dir, layer="networks-proposed")
     # TODO: re-project metrics_csv and network_shp to same PROJ? (which one?)
      
-    # Code to create graph from network_shp
-    # TODO: clean up
+    segment_matrix <- get_segment_matrix(network_shp)
+    network <- create_graph(metrics_df, segment_matrix) 
 
-    line_matrix <- get_coord_matrix(network_shp)
-    coord_df <- get_coord_dataframe(network_shp)
-    network_adj_matrix <- get_adjacency_matrix(line_matrix, coord_df)
-    network_graph <- graph.adjacency(network_adj_matrix, mode="undirected")
-
-    V(network_graph)$name <- coord_df$id
-    V(network_graph)$type <- letters[1:2:3]
-    
-    V(network_graph)[V(network_graph)$type == "a"]
-    
-    plot(network_graph, vertex.size=2)
-    
-    nwg_df <- get.data.frame(network_graph, what=c("vertices"))
-    edg_df <- get.data.frame(network_graph, what=c("edges"))
-
-    nwg_df <- merge(nwg_df, coord_df, by.x="name", by.y="id")
-    nwg_df <- merge(nwg_df, metrics_csv, by.x=c("x", "y"), by.y=c("X", "Y"), all.x=TRUE)
-    
-    new_graph <- graph.data.frame(edg_df, directed=FALSE)
-
-    plot(new_graph, vertex.size=2)
-
-
-    ## TODO: figure out intersection points, with the following objectives:
+    ## TODO: 
+    ## Find all "fake" nodes
+    fake_vids <- V(network)[is.na(V(network)$nid)]$vid
+    reachable_from_fake <- unique(do.call(c, lapply(fake_vids, function(x) { subcomponent(network, x, mode="ALL") })))
+    # reachable_from_fake <- subcomponent(network, fake_vids, mode="ALL")
+    unreachable <- setdiff(as.numeric(V(network)), reachable_from_fake)
+    unreachable_subgraph <- subgraph(network, unreachable)
+      
+    ## Create dominator.tree for each fake node
+    ## Handle subnets without "fake" nodes
     ## determine is_root for each node
     ## determine which parts of network_shp go into network::igraph and existing_network::SpatialLinesDataFrame
-    new("NetworkPlan", nodes=nodes, network=network_graph)
+    new("NetworkPlan", nodes=nodes, network=network)
 }
 
 
--- R/np-util.R
+++ R/np-util.R
@@ -1,4 +1,3 @@
-
 # assign id, x, y to all points in SpatialLinesDataFrame
 get_coord_dataframe <- function(sldf) {
     coord_list <- llply(sldf@lines, function(l) { l@Lines[[1]]@coords })
@@ -10,18 +9,18 @@ get_coord_dataframe <- function(sldf) {
     
 # get adjacency matrix rep of sldf referencing coord_df ids
 # TODO:  Not sure why I get some cycles with this method
-get_adjacency_matrix <- function(line_matrix, coord_df) {
-    p1 <- as.data.frame(line_matrix[1:nrow(line_matrix),1,1:2])
-    p2 <- as.data.frame(line_matrix[1:nrow(line_matrix),2,1:2])
+get_adjacency_matrix <- function(segment_matrix, segment_node_df) {
+    p1 <- as.data.frame(segment_matrix[1:nrow(segment_matrix),1,1:2])
+    p2 <- as.data.frame(segment_matrix[1:nrow(segment_matrix),2,1:2])
     # need the ids to ensure order after the merge
     p1$pid <- as.numeric(row.names(p1))
     p2$pid <- as.numeric(row.names(p2))
-    p1_coord_merge <- merge(p1, coord_df, by.x=c(1,2), by.y=c("x","y"))[,c("pid", "id")]
-    p2_coord_merge <- merge(p2, coord_df, by.x=c(1,2), by.y=c("x","y"))[,c("pid", "id")]
+    p1_coord_merge <- merge(p1, segment_node_df, by.x=c(1,2), by.y=c("X","Y"))[,c("pid", "id")]
+    p2_coord_merge <- merge(p2, segment_node_df, by.x=c(1,2), by.y=c("X","Y"))[,c("pid", "id")]
     # now get the node ids in the correct order
     p1_ids <- p1_coord_merge[with(p1_coord_merge, order(pid)),"id"] 
     p2_ids <- p2_coord_merge[with(p2_coord_merge, order(pid)),"id"] 
-    adj_matrix <- matrix(nrow=nrow(coord_df), ncol=nrow(coord_df), data=0)
+    adj_matrix <- matrix(nrow=nrow(segment_node_df), ncol=nrow(segment_node_df), data=0)
     
     # must be a better way to set the adjacency matrix incidence cells
     # but for now, use index_matrices (both from/to and to/from)
@@ -37,7 +36,7 @@ get_adjacency_matrix <- function(line_matrix, coord_df) {
 
 # Get the co-ordinate matrix out of a SpatialLinesDataFrame (as a three dimensional array)
 # Invariant: we should be connecting straight lines in 2D space (ie, 2nd + 3rd dims are 2)
-get_coord_matrix = function(sldf) {
+get_segment_matrix = function(sldf) {
     # Looping through every line slot in SLDF object 
     # and save the 2X2 matrix representation of a single edge into 2 3D array. 
     line_coords <- lapply(sldf@lines, function(l) {
@@ -69,6 +68,66 @@ get_coord_matrix = function(sldf) {
     return(line_coords)
 }
 
+ 
+test_edge_pairs <- function(segment_node_df, p1, p2, network) {
+    p1_p2 <- data.frame(cbind(p1, p2))
+    names(p1_p2) <- c("X1", "Y1", "X2", "Y2")
+    p1_p2 <- merge(p1_p2, segment_node_df, by.x=c("X1", "Y1"), by.y=c("X", "Y"), all.x=TRUE)
+    p1_p2 <- merge(p1_p2, segment_node_df, by.x=c("X2", "Y2"), by.y=c("X", "Y"), all.x=TRUE)
+    edge_df <- get.data.frame(network, what="edges")
+    # careful not to test this with large graphs (cartesion product)
+    result <- merge(p1_p2, edge_df)
+    result <- subset(result, (id.x==from & id.y==to) | (id.x==to & id.y==from))
+    expect_equal(nrow(p1_p2)==nrow(result))
+    expect_equal(nrow(p1_p2)==(2*nrow(edge_df)))
+}
+   
+# create graph from node_dataframe (NOT sp) and segment_matrix
+create_graph <- function(metrics_df, segment_matrix) {
+    
+    p1 <- segment_matrix[1:dim(segment_matrix)[1],1,1:2]
+    p2 <- segment_matrix[1:dim(segment_matrix)[1],2,1:2]
+    segment_node_df<- data.frame(unique(rbind(p1,p2)))
+    names(segment_node_df) <- c("X", "Y")
+    segment_node_df$id <- as.numeric(row.names(segment_node_df))
+ 
+    network_adj_matrix <- get_adjacency_matrix(segment_matrix, segment_node_df)
+    network <- graph.adjacency(network_adj_matrix, mode="undirected")
+    #vid and vertex index match. add explicit vid field to track
+    V(network)$vid <- segment_node_df$id
+
+    # now assign metrics_df attributes back to vertices
+    # first, make sure that metrics_df has an nid, representing
+    # a unique id for the node (will make it easier to distinguish "fake" nodes)
+    metrics_df$nid <- as.numeric(row.names(metrics_df))
+    vertex_df <- get.data.frame(network, what=c("vertices"))
+    # first get the x,y's to join via
+    vertex_df <- merge(vertex_df, segment_node_df, by.x="vid", by.y="id")
+    # join to metrics_df and keep all values from vertex_df
+    vertex_df <- merge(vertex_df, metrics_df, by.x=c("X","Y"), by.y=c("X","Y"), all.x=TRUE)
+    # should be same size (vertices were created from same set of nodes in segment_node_df)
+    stopifnot(nrow(vertex_df)==nrow(segment_node_df))
+    
+    edge_df <- get.data.frame(network, what="edges")
+    #reorder vertex df names so that vid is 1st (required to associate with correct edge)
+    vertex_names <- c(c("vid"), setdiff(names(vertex_df), c("vid")))
+    # vertex_names <- c(c("vid"), names(vertex_df))
+    # Because the "vid" column gets lost when it's used as the 1st "linking"
+    # column by graph.data.frame, we need to duplicate this column and rename
+    # it
+    vertex_df <- vertex_df[,vertex_names]
+    names(vertex_df)[1] <- "edge_link_id"
+    vertex_df$vid <- vertex_df$edge_link_id
+    
+    #TODO:  do we want directed/undirected here?
+    network <- graph.data.frame(edge_df, directed=FALSE, vertex_df)
+    network
+}
+    
+#     # Merge with node df and detecting ghost nodes & roots
+# #     t1 <- match(data.frame(t(p1)), data.frame(t(nodes@coords)))
+# #     t2 <- match(data.frame(t(p2)), data.frame(t(nodes@coords)))
+# #     match_result <- cbind(t1,t2)
 
 # Depricated get_coord_matrix fucntion keeping here for testing purpose for now
 # TO DO: turn it into test
@@ -119,3 +178,5 @@ get_coord_matrix = function(sldf) {
 # 
 # summary(bench_mark)
 # qplot(y=time, data=bench_mark, colour=expr) + scale_y_log10()
+
+tmp <- function() { 1 }
