f416c5e6d1b0b8b464abf830be87643715d3319b
zmyao88 <zm.yao88@gmail.com>
2014-04-01 14:32:29 -0400

    function update

--- R/networkplanner.R
+++ R/networkplanner.R
@@ -6,7 +6,6 @@ require(plyr)
 require(abind)
 require(maptools)
 require(RCurl)
-require(R.utils)
 
 #' @include util.R
 #' @include sequence_models.R 
@@ -53,7 +52,7 @@ download_scenario = function(scenario_number, directory_name=NULL, username=NULL
     }
     
     # Standardize/ convert to absolute path
-    base_dir <- getAbsolutePath(directory_name)
+    base_dir <- R.utils::getAbsolutePath(directory_name)
         
     # Create a Boolean flag indicating if the repo if private
     # error handling for only 1 NULL value for user & pass
@@ -106,7 +105,7 @@ download_scenario = function(scenario_number, directory_name=NULL, username=NULL
 #' @return A NetworkPlan object
 #' @export
 read_networkplan = function(directory_name, debug=F) {
-    base_dir = getAbsolutePath(directory_name)
+    base_dir = R.utils::getAbsolutePath(directory_name)
     
     # read nodes and assign id
     metrics_df <- read.csv(file.path(base_dir, "metrics-local.csv"), skip=1)
@@ -294,7 +293,7 @@ setMethod("accumulate", signature(np="NetworkPlan"),
 write.NetworkPlan = function(np, directory_name, 
                              nodeFormat='csv', edgeFormat='shp', includeFake=FALSE) {
     
-    base_dir <- getAbsolutePath(directory_name)
+    base_dir <- R.utils::getAbsolutePath(directory_name)
     
     # subsetting node_df according to includeFake
     node_df <- get.data.frame(np@network, what="vertices")
